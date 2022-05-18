e4f257b519bf0c813535cfffad418ceb59a4579a
leconman <Connor.Courtien83@myhunter.cuny.edu>
2022-05-18 01:44:14 -0400

    added view for adding an instructor

--- src/components/views/NewCourseView.js
+++ src/components/views/NewCourseView.js
@@ -23,6 +23,11 @@ const NewCourseView = (props) => {
           <br/>
           <br/>
 
+          <label style={{color:'#11153e', fontWeight: 'bold'}}>Location: </label>
+          <input type="text" name="location" onChange={(e) => handleChange(e)} />
+          <br/>
+          <br/>
+
           <label style={{color:'#11153e', fontWeight: 'bold'}}>instructorId: </label>
           <input type="text" name="instructorId" onChange={(e) => handleChange(e)} />
           <br/>
